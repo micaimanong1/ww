e023742aa4b7024efcf5d027efe1a316eb9cec11
ssbgxwj@163.com <MCMN@DESKTOP-N466656>
2018-12-11 22:19:18 +0800

    old2

--- aa/src/main/java/aa/t.java
+++ aa/src/main/java/aa/t.java
@@ -7,7 +7,13 @@ public static void main(String[] args) {
 	System.out.println("11");
 	System.out.println(	Thread.currentThread().getName());
 	
+	System.out.println(" old+ test ");
+	
+	
 	System.out.println(" old ");
 	System.out.println("t.main()");
+	
+	
+	System.out.println("old +1");
 }
 }
