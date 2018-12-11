88130621e05fdd92451237f711ef9f247661fa8f
ssbgxwj@163.com <MCMN@DESKTOP-N466656>
2018-12-11 21:57:36 +0800

    3

--- aa/src/main/java/aa/t.java
+++ aa/src/main/java/aa/t.java
@@ -8,6 +8,6 @@ public static void main(String[] args) {
 	System.out.println(	Thread.currentThread().getName());
 	
 	System.out.println(" old ");
-	System.out.println("t.main();
+	System.out.println("t.main()");
 }
 }
