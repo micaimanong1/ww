247f6018b902ca499f900006619959eadc50883e
MCMN <MCMN@DESKTOP-N466656>
2018-12-11 21:05:38 +0800

    testcomit1

--- aa/src/main/java/aa/t.java
+++ aa/src/main/java/aa/t.java
@@ -1,7 +1,10 @@
 package aa;
 
+import sun.tools.tree.ThisExpression;
+
 public class t {
 public static void main(String[] args) {
 	System.out.println("11");
+	System.out.println(	Thread.currentThread().getName());
 }
 }
